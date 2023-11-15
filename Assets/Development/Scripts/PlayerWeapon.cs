de3a84433deee66c6801c98c8ccc3906c5d0ba14
Anh "Anthony" Nguyen <anthony.trianh@gmail.com>
2023-11-15 15:40:01 -0500

    Fixed screen-mouse weapon aiming

--- Assets/Development/Scripts/PlayerWeapon.cs
+++ Assets/Development/Scripts/PlayerWeapon.cs
@@ -1,7 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
+using UnityEditor;
 //using UnityEditor;
 using UnityEngine;
+using UnityEngine.Rendering.VirtualTexturing;
+using Matrix4x4 = UnityEngine.Matrix4x4;
+using Plane = UnityEngine.Plane;
+using Quaternion = UnityEngine.Quaternion;
+using Vector3 = UnityEngine.Vector3;
 
 public class PlayerWeapon : MonoBehaviour
 {
@@ -64,13 +72,23 @@ public class PlayerWeapon : MonoBehaviour
     public void UpdateWeaponRotation()
     {
         Ray camera = cam.ScreenPointToRay(Input.mousePosition);
-        Plane ground = new Plane(Vector3.up, Vector3.zero);
+        // A single point on the plane where the weapon is positioned (in world-space)
+        //  in this case, we just want the height of the weapon
+        weaponPlanePoint = transform.position;
+        // Weapon's plane normal, determines the plane's rotation
+        // *Notes: In theory, this value can be changed to achieve weird planes with can result
+        //  in different rotations and what not for an attack... but for now we'll use up vector
+        //  as the standard
+        planeNormal = Vector3.up;
+        
+        Plane ground = new Plane(planeNormal, weaponPlanePoint);
         float length;
 
         if (ground.Raycast(camera, out length))
         {
             Vector3 look = camera.GetPoint(length);
-            transform.LookAt(new Vector3(look.x, transform.position.y, look.z));
+            raycastHit = new Vector3(look.x, transform.position.y, look.z);
+            transform.LookAt(raycastHit);
 
             // Find Yaw rotation angle and update animator based on that
             Quaternion aimRotation = transform.rotation;
@@ -78,48 +96,88 @@ public class PlayerWeapon : MonoBehaviour
         }
     }
 
-    /*
-    private void OnDrawGizmosSelected()
-    {
-        Gizmos.color = Color.red;
-        DrawWireCapsule(weaponStart.position, weaponEnd.position, weaponRadius, 0, Color.red);
-    }
-    #region Gizmos helper
+    Vector3 planeNormal;
+    Vector3 weaponPlanePoint;
+    Vector3 raycastHit;
+    
 #if UNITY_EDITOR
-    public static void DrawWireCapsule(Vector3 _pos, Vector3 _pos2, float _radius, float _height, Color _color = default)
+    void OnDrawGizmos()
     {
-        if (_color != default) Handles.color = _color;
- 
-        var forward = _pos2 - _pos;
-        var _rot = Quaternion.LookRotation(forward);
-        var pointOffset = _radius/2f;
-        var length = forward.magnitude;
-        var center2 = new Vector3(0f,0,length);
-       
-        Matrix4x4 angleMatrix = Matrix4x4.TRS(_pos, _rot, Handles.matrix.lossyScale);
-       
-        using (new Handles.DrawingScope(angleMatrix))
-        {
-            Handles.DrawWireDisc(Vector3.zero, Vector3.forward, _radius);
-            Handles.DrawWireArc(Vector3.zero, Vector3.up, Vector3.left * pointOffset, -180f, _radius);
-            Handles.DrawWireArc(Vector3.zero, Vector3.left, Vector3.down * pointOffset, -180f, _radius);
-            Handles.DrawWireDisc(center2, Vector3.forward, _radius);
-            Handles.DrawWireArc(center2, Vector3.up, Vector3.right * pointOffset, -180f, _radius);
-            Handles.DrawWireArc(center2, Vector3.left, Vector3.up * pointOffset, -180f, _radius);
-           
-            DrawLine(_radius,0f,length);
-            DrawLine(-_radius,0f,length);
-            DrawLine(0f,_radius,length);
-            DrawLine(0f,-_radius,length);
-        }
-    }
- 
-    private static void DrawLine(float arg1,float arg2,float forward)
-    {
-        Handles.DrawLine(new Vector3(arg1, arg2, 0f), new Vector3(arg1, arg2, forward));
+            Handles.color = Color.red;
+            Vector3 a = raycastHit;
+            Vector3 b = raycastHit + Vector3.up * 1f;
+            Handles.DrawAAPolyLine(a, b);
+
+            DrawPlane(transform.position, planeNormal);
     }
 #endif
-    #endregion
-    */
+
+    public void DrawPlane(Vector3 position, Vector3 normal)
+    {
+        Vector3 v3;
+
+        if (normal.normalized != Vector3.forward)
+            v3 = Vector3.Cross(normal, Vector3.forward).normalized * normal.magnitude;
+        else
+            v3 = Vector3.Cross(normal, Vector3.up).normalized * normal.magnitude; ;
+
+        var corner0 = position + v3;
+        var corner2 = position - v3;
+        var q = Quaternion.AngleAxis(90.0f, normal);
+        v3 = q * v3;
+        var corner1 = position + v3;
+        var corner3 = position - v3;
+
+        Debug.DrawLine(corner0, corner2, Color.green);
+        Debug.DrawLine(corner1, corner3, Color.green);
+        Debug.DrawLine(corner0, corner1, Color.green);
+        Debug.DrawLine(corner1, corner2, Color.green);
+        Debug.DrawLine(corner2, corner3, Color.green);
+        Debug.DrawLine(corner3, corner0, Color.green);
+        Debug.DrawRay(position, normal, Color.red);
+    }
+
+    //     private void OnDrawGizmosSelected()
+//     {
+//         Gizmos.color = Color.red;
+//         DrawWireCapsule(weaponStart.position, weaponEnd.position, weaponRadius, 0, Color.red);
+//     }
+//     #region Gizmos helper
+// #if UNITY_EDITOR
+//     public static void DrawWireCapsule(Vector3 _pos, Vector3 _pos2, float _radius, float _height, Color _color = default)
+//     {
+//         if (_color != default) Handles.color = _color;
+//  
+//         var forward = _pos2 - _pos;
+//         var _rot = Quaternion.LookRotation(forward);
+//         var pointOffset = _radius/2f;
+//         var length = forward.magnitude;
+//         var center2 = new Vector3(0f,0,length);
+//        
+//         Matrix4x4 angleMatrix = Matrix4x4.TRS(_pos, _rot, Handles.matrix.lossyScale);
+//        
+//         using (new Handles.DrawingScope(angleMatrix))
+//         {
+//             Handles.DrawWireDisc(Vector3.zero, Vector3.forward, _radius);
+//             Handles.DrawWireArc(Vector3.zero, Vector3.up, Vector3.left * pointOffset, -180f, _radius);
+//             Handles.DrawWireArc(Vector3.zero, Vector3.left, Vector3.down * pointOffset, -180f, _radius);
+//             Handles.DrawWireDisc(center2, Vector3.forward, _radius);
+//             Handles.DrawWireArc(center2, Vector3.up, Vector3.right * pointOffset, -180f, _radius);
+//             Handles.DrawWireArc(center2, Vector3.left, Vector3.up * pointOffset, -180f, _radius);
+//            
+//             DrawLine(_radius,0f,length);
+//             DrawLine(-_radius,0f,length);
+//             DrawLine(0f,_radius,length);
+//             DrawLine(0f,-_radius,length);
+//         }
+//     }
+//  
+//     private static void DrawLine(float arg1,float arg2,float forward)
+//     {
+//         Handles.DrawLine(new Vector3(arg1, arg2, 0f), new Vector3(arg1, arg2, forward));
+//     }
+// #endif
+//     #endregion
+//     
 }
 
