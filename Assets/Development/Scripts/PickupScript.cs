b3af8370ec4f38de2fa2d87bf55de26dd46981e1
Daniel Fevriere <dfevriere.df@gmail.com>
2024-12-08 23:16:15 -0500

    Pickup prototype
    - green collider in the dev test scene gets picked up

---
+++ Assets/Development/Scripts/PickupScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupScript : MonoBehaviour
+{
+    public bool pickupable;
+    [SerializeField] RewardGiver itemPickup;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            //Make sure to eventually add UI feedback
+            itemPickup.GiveReward();
+            Destroy(gameObject);
+        }
+    }
+}
